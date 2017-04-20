5cdf7c039f3c62865cc8068c04179a7898bda32f
MorelAntoine <antoinemorel@outlook.com>
2017-04-20 22:32:21 +0200

    send result by internet -ok

--- Salad-Me/MainActivity.cs
+++ Salad-Me/MainActivity.cs
@@ -6,36 +6,84 @@ using Android.OS;
 using Android.Runtime;
 using Android.Graphics;
 using System;
+using System.Net;
+using System.Json;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace Salad_Me
 {
     [Activity(Label = "Salad_Me", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        EditText editText;
+        TextView whiteText;
+        TextView necText;
+        Button netBtn;
+        Button nfcBtn;
+        Image img;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView (Resource.Layout.Main);
-
-            Button btnCamera = FindViewById<Button>(Resource.Id.btnCamera);
-            editText = FindViewById<EditText>(Resource.Id.editText);
-
+            Button btnCamera = FindViewById<Button>(Resource.Id.startSessionButton);
+            whiteText = FindViewById<TextView>(Resource.Id.whiteText);
+            necText = FindViewById<TextView>(Resource.Id.necText);
+            netBtn = FindViewById<Button>(Resource.Id.netButton);
+            nfcBtn = FindViewById<Button>(Resource.Id.nfcButton);
             btnCamera.Click += BtnCameraClik;
+            netBtn.Click += (sender, e) => {
+                string url = "https://salad-me.firebaseio.com/vegetablesData.json";
+                FetchResults(url, img);
+            };
         }
-
-        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-            Bitmap bitmap = (Bitmap)(data.Extras.Get("data"));
-            Image img = new Image(bitmap);
-            editText.Text = "Contient : " + img.getWhite().ToString() + "% de blanc !";
-        }
-
         private void BtnCameraClik(object sender, EventArgs e)
         {
             Intent intent = new Intent(MediaStore.ActionImageCapture);
             StartActivityForResult(intent, 0);
+        }
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap bitmap = (Bitmap)(data.Extras.Get("data"));
+            img = new Image(bitmap);
+            whiteText.Text = img.getWhite().ToString();
+            necText.Text = img.getNecrose().ToString();
+            netBtn.Enabled = true;
+            nfcBtn.Enabled = true;
+        }
+
+        private void FetchResults(string url, Image img)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            string data =
+                "{\"farmerId\": 1," +
+                " \"type\": 1," +
+                "\"position\": {\"lat\": \"48.862725\",\"lng\": \"2.287592000000018\"}," +
+                "\"date\": 1211929182," +
+                "\"white\": " + img.getWhite().ToString() + "," +
+                "\"necrosity\": " + img.getNecrose().ToString() + "}";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+                streamWriter.Flush();
+                streamWriter.Close();
+                try
+                {
+                    var httpResponse = (HttpWebResponse)request.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        Console.Out.WriteLine("Response Body: \r\n {0}", result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.WriteLine("Exception {0}", ex);
+                }
+            }
         }
     }
 }
--- Salad-Me/Resources/Resource.Designer.cs
+++ Salad-Me/Resources/Resource.Designer.cs
@@ -60,14 +60,47 @@ namespace Salad_Me
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int btnCamera = 2131034114;
-			
 			// aapt resource value: 0x7f050001
-			public const int editText = 2131034113;
+			public const int getSection = 2131034113;
+			
+			// aapt resource value: 0x7f050007
+			public const int humidSection = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int humidText = 2131034121;
 			
 			// aapt resource value: 0x7f050000
-			public const int imgView = 2131034112;
+			public const int latSection = 2131034112;
+			
+			// aapt resource value: 0x7f050008
+			public const int necLabel = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int necText = 2131034122;
+			
+			// aapt resource value: 0x7f05000c
+			public const int netButton = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int nfcButton = 2131034125;
+			
+			// aapt resource value: 0x7f050002
+			public const int startSessionButton = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int tempSection = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int tempText = 2131034117;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView1 = 2131034123;
+			
+			// aapt resource value: 0x7f050004
+			public const int whiteLabel = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int whiteText = 2131034118;
 			
 			static Id()
 			{
